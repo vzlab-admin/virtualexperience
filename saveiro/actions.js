0dd88499cd3c3b6b969710d627f3ab81644cff6c
Marcelo Arteiro <marteiro@me.com>
2016-11-01 12:52:43 +0000

    Atualizações de CSS e fontes no formulario da Saveiro

--- saveiro/actions.js
+++ saveiro/actions.js
@@ -142,3 +142,39 @@ var verificaForm = function(form, callback) {
         message: "Alguma mensagem do server."
     });
 }
+
+function sendData(data, URL) {
+    var XHR = new XMLHttpRequest();
+    var urlEncodedData = "";
+    var urlEncodedDataPairs = [];
+    var name;
+
+    // We turn the data object into an array of URL encoded key value pairs.
+    for (name in data) {
+        urlEncodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
+    }
+
+    // We combine the pairs into a single string and replace all encoded spaces to 
+    // the plus character to match the behaviour of the web browser form submit.
+    urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
+
+    // We define what will happen if the data is successfully sent
+    XHR.addEventListener('load', function(event) {
+        alert('Yeah! Data sent and response loaded.');
+    });
+
+    // We define what will happen in case of error
+    XHR.addEventListener('error', function(event) {
+        alert('Oups! Something goes wrong.');
+    });
+
+    // We setup our request
+    XHR.open('POST', URL);
+
+    // We add the required HTTP header to handle a form data POST request
+    XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    XHR.setRequestHeader('Content-Length', urlEncodedData.length);
+
+    // And finally, We send our data.
+    XHR.send(urlEncodedData);
+}
